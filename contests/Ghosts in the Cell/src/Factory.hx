f64a654503ace3f3b0caac47356e719973062847
0xA3 <deslac@protonmail.ch>
2020-06-05 10:30:03 +0200

    calculate factory scores

--- contests/Ghosts in the Cell/src/Factory.hx
+++ contests/Ghosts in the Cell/src/Factory.hx
@@ -1,23 +1,148 @@
 import CodinGame.printErr;
 
+using Lambda;
+
+enum abstract Action(Int) {
+	var Take;
+	var Attack;
+	var Defend;
+	var Increase;
+}
+
+enum TroopsToSend {
+	All;
+	Some( troops:Int );
+}
+
 class Factory {
 	
 	public final id:Int;
+	public var owner = 0;
 	public var cyborgs = 0;
 	public var production = 0;
 	public var value = 0.0;
+	public var score = 0.0;
+	public final myIncomingTroops:Array<Troop> = [];
+	public final incomingEnemyTroops:Array<Troop> = [];
+
+	public var action:Action;
+	public var troopsToSend = Some( 0 );
 
 	public function new( id:Int ) {
 		this.id = id;
 	}
 
-	public function update( cyborgs:Int, production:Int ) {
+	public function reset() {
+		incomingEnemyTroops.splice( 0, incomingEnemyTroops.length );
+	}
+
+	public function update( owner:Int, cyborgs:Int, production:Int ) {
+		this.owner = owner;
 		this.cyborgs = cyborgs;
 		this.production = production;
 	}
 
-	public function setValue( pathsThrough:Int ) {
+	public function calculateValue( pathsThrough:Int ) {
 		value = production + 0.01 * Math.pow( pathsThrough, 0.5 ) + 0.1;
 		// printErr( 'factory $id cybs $cyborgs prod $production value $value' );
 	}
+
+	public function addMyIncomingTroop( troop:Troop ) {
+		myIncomingTroops.push( troop );
+	}
+	
+	public function addIncomingEnemyTroop( troop:Troop ) {
+		incomingEnemyTroops.push( troop );
+	}
+
+	public function getNeededForDefense( turns:Int ) {
+		final attackers = incomingEnemyTroops.fold(( troop, sum ) -> troop.arrives <= turns ? troop.cyborgs : 0, 0 );
+		return attackers;
+	}
+
+	function getNeededReinforcements( turns:Int ) {
+		final attackers = getNeededForDefense( turns );
+		final producted = turns * production;
+		return Std.int( Math.max( 0, attackers - cyborgs - producted ));
+		// todo account for incoming bombs
+	}
+
+	function getRequiredToTakeNeutral( turnsToGetThere:Int ) {
+		final fasterEnemyTroops = incomingEnemyTroops.filter( troop -> troop.arrives <= turnsToGetThere );
+		final enemyTroops = fasterEnemyTroops.fold(( troop, sum ) -> sum + troop.cyborgs, 0 );
+		if( enemyTroops > cyborgs ) {
+			fasterEnemyTroops.sort( Troop.sortByArrives );
+			var turnsUntilTakeover = 0;
+			var neutralCyborgs = cyborgs;
+			for( troop in fasterEnemyTroops ) {
+				neutralCyborgs -= troop.cyborgs;
+				turnsUntilTakeover = troop.arrives;
+				if( neutralCyborgs < 0 ) break;
+			}
+			final remainingEnemyTroops = enemyTroops - cyborgs;
+			final productionAfterTakeOver = (turnsToGetThere - turnsUntilTakeover ) * production;
+			return remainingEnemyTroops + productionAfterTakeOver;
+		} else {
+			return cyborgs;
+		}
+		
+	}
+
+	public function calculateScore( turnsToGetThere:Int ) {
+		switch owner {
+			case 1:
+				final defenseScore = calculateDefenseScore( turnsToGetThere );
+				final increaseScore = calculateIncreaseScore();
+				if( defenseScore > increaseScore ) {
+					score = defenseScore;
+					action = Defend;
+				} else {
+					score = increaseScore;
+					action = Increase;
+				}
+			case -1:
+				calculateEnemyAttackScore( turnsToGetThere );
+				action = Attack;
+			default:
+				calculateNeutralScore( turnsToGetThere );
+				action = Take;
+		}
+	}
+
+	function calculateNeutralScore( turnsToGetThere:Int ) {
+		final troops = getRequiredToTakeNeutral( turnsToGetThere );
+		troopsToSend = Some( troops );
+		final s = value / Math.pow( turnsToGetThere, 2 ) * troops;
+		// printErr( '$id neutral troops $troops $score $s' );
+		return s;
+	}
+
+	function calculateEnemyAttackScore( turnsToGetThere:Int ) {
+		troopsToSend = All;
+		final s = value / ( Math.pow( turnsToGetThere, 2 ) * 8 );
+		// printErr( '$id enemy $score $s' );
+		return s;
+	}
+
+	function calculateDefenseScore( turnsToGetThere:Int ) {
+		final troops = getNeededReinforcements( turnsToGetThere + 5 );
+		troopsToSend = Some( troops );
+		final s = value / ( Math.pow( turnsToGetThere, 2 ) * troops );
+		// printErr( '$id my score $s' );
+		return s;
+	}
+
+	function calculateIncreaseScore() {
+		action = Increase;
+		final s = 1 / Math.pow( 10, 1.6 );
+		// printErr( '$id my increase $s' );
+		return s;
+	}
+
+	public static function sortByScore( f1:Factory, f2:Factory ) {
+		if( f1.score > f2.score ) return -1;
+		if( f1.score < f2.score ) return 1;
+		return 0;
+	}
+
 }
--- contests/Ghosts in the Cell/src/Main.hx
+++ contests/Ghosts in the Cell/src/Main.hx
@@ -54,12 +54,14 @@ class Main {
 		var turn = 0;
 		// game loop
 		while( true ) {
+			for( factory in factories ) factory.reset();
+			
 			final entityCount = parseInt( readline()); // the number of entities( e.g. factories and troops )
 			
 			myFactories.splice( 0, myFactories.length );
 			enemyFactories.splice( 0, enemyFactories.length );
 			neutralFactories.splice( 0, neutralFactories.length );
-			
+
 			for( _ in 0...entityCount ) {
 				final inputs = readline().split( ' ' );
 				final entityId = parseInt( inputs[0] );
@@ -69,62 +71,82 @@ class Main {
 				final arg3 = parseInt( inputs[4] );
 				final arg4 = parseInt( inputs[5] );
 				final arg5 = parseInt( inputs[6] );
-				if( entityType == "FACTORY" ) {
-					final factory = factoriesMap[entityId];
-					factory.update( arg2, arg3 );
-					// printErr( 'id ${factory.id} owner $arg1 cybs ${factory.cyborgs} prod ${factory.production}' );
-					switch arg1 {
-						case 1: myFactories.push( factory );
-						case -1: enemyFactories.push( factory );
-						default: neutralFactories.push( factory );
-					}
+				switch entityType {
+					case "FACTORY":
+						// arg1: player that owns the factory: 1 for you, -1 for your opponent and 0 if neutral
+						// arg2: number of cyborgs in the factory
+						// arg3: factory production (between 0 and 3)
+						// arg4: number of turns before the factory starts producing again (0 means that the factory produces normally)
+						// arg5: unused	
+						final factory = factoriesMap[entityId];
+						factory.update(  arg1, arg2, arg3 );
+						// printErr( 'id ${factory.id} owner $arg1 cybs ${factory.cyborgs} prod ${factory.production}' );
+						switch arg1 {
+							case 1: myFactories.push( factory );
+							case -1: enemyFactories.push( factory );
+							default: neutralFactories.push( factory );
+						}
+					case "TROOP":
+						// arg1: player that owns the troop: 1 for you or -1 for your opponent
+						// arg2: identifier of the factory from where the troop leaves
+						// arg3: identifier of the factory targeted by the troop
+						// arg4: number of cyborgs in the troop (positive integer)
+						// arg5: remaining number of turns before the troop arrives (positive integer)
+						final troop = new Troop( arg2, arg3, arg4, arg5 );
+						final targetFactory = factoriesMap[troop.targetFactory];
+						arg1 == 1 ? targetFactory.addMyIncomingTroop( troop ) :	targetFactory.addIncomingEnemyTroop( troop );
+					case _: // BOMB
+						// arg1: player that send the bomb: 1 if it is you, -1 if it is your opponent
+						// arg2: identifier of the factory from where the bomb is launched
+						// arg3: identifier of the targeted factory if it's your bomb, -1 otherwise
+						// arg4: remaining number of turns before the bomb explodes (positive integer) if that's your bomb, -1 otherwise
+						// arg5: unused					
 				}
 			}
 		
 			// init factory value
-			if( turn == 0 ) for( factory in factories ) factory.setValue( pathsThrough[factory.id] );
+			if( turn == 0 ) for( factory in factories ) factory.calculateValue( pathsThrough[factory.id] );
 
-			if( myFactories.length == 0 || enemyFactories.length == 0 ) {
-				print( 'WAIT' );
-			} else {
-				myFactories.sort( inverseSortByCyborgs );
-				
-				final myFactoryId = myFactories[0].id;
-				final troops = myFactories[0].cyborgs;
-				
-				if( neutralFactories.length > 0 ) {
-					
-					final factoryDistances:Array<FactoryDistance> = neutralFactories.map( factory -> { id:factory.id, distance: shortestPaths.get( '$myFactoryId-${factory.id}' ).length} );
-					factoryDistances.sort( sortByDistance );
-					final targetFactory = factoryDistances[0].id;
-					print( 'MOVE $myFactoryId $targetFactory ${Std.int( troops / 2 )}' );
-
-				} else {
-					
-					final randomEnemyFactory = enemyFactories[Std.random( enemyFactories.length )].id;
-					print( 'MOVE $myFactoryId $randomEnemyFactory $troops' );
+			final moves:Array<String> = [];
+			for( myFactory in myFactories ) {
+				myFactory.score = 0;
+				for( other in factories ) {
+					if( other.id != myFactory.id ) {
+						final pathId = '${myFactory.id}-${other.id}';
+						final turnsToGetThere = Std.int( shortestPaths[pathId].length );
+						other.calculateScore( turnsToGetThere );
+					}
 				}
+				factories.sort( Factory.sortByScore );
+				// for( other in factories ) {
+				// 	printErr( '${myFactory.id}-${other.id} score ${other.score}' );
+				// }
+				var sparableForces = myFactory.cyborgs - myFactory.getNeededForDefense( 5 );
+				// printErr( 'sparableForces $sparableForces' );
+				if( sparableForces > 0 ) {
+					for( other in factories ) {
+						if( other.id != myFactory.id ) {
+							final pathId = '${myFactory.id}-${other.id}';
+							final path = shortestPaths[pathId];
+							
+							final troopsToSend = switch other.troopsToSend {
+								case All: sparableForces;
+								case Some( troopsToSend ): Std.int( Math.max( 0, Math.min( sparableForces, troopsToSend )));
+							}
+							moves.push( 'MOVE ${myFactory.id} ${path.edges[0].to} $troopsToSend' );
+							sparableForces -= troopsToSend;
+							if( sparableForces <= 0 ) break;
+						}
+					}
+				}
+			}
+			if( moves.length > 0 ) {
+				print( moves.join( ";" ));
+			} else {
+				print( "WAIT" );
 			}
 			turn++;
 		}
-	}
-
-	static function sortByCyborgs( f1:Factory, f2:Factory ) {
-		if( f1.cyborgs > f2.cyborgs ) return 1;
-		if( f1.cyborgs < f2.cyborgs ) return -1;
-		return 0;
-	}
-
-	static function inverseSortByCyborgs( f1:Factory, f2:Factory ) {
-		if( f1.cyborgs > f2.cyborgs ) return -1;
-		if( f1.cyborgs < f2.cyborgs ) return 1;
-		return 0;
-	}
-
-	static function sortByDistance( d1:FactoryDistance, d2:FactoryDistance ) {
-		if( d1.distance > d2.distance ) return 1;
-		if( d1.distance < d2.distance ) return -1;
-		return 0;
 	}
 
 }
