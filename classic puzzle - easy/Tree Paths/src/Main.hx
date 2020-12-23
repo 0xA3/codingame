a76832cb259270a6e1e001dde65e0478531ad28e
0xA3 <deslac@protonmail.ch>
2020-12-23 12:05:04 +0100

    Improve Tree Paths

--- classic puzzle - easy/Tree Paths/src/Main.hx
+++ classic puzzle - easy/Tree Paths/src/Main.hx
@@ -13,47 +13,41 @@ class Main {
 		final n = parseInt( readline()); // number of nodes in the tree
 		final v = parseInt( readline()); // index of the target node
 		final m = parseInt( readline()); // number of nodes with two children
-
+		
 		final nodes:Map<Int, Node> = [];
+		final parents:Map<Int, Int> = [];
 
 		for( i in 0...m ) {
 			final inputs = readline().split(' ');
 			final p = parseInt( inputs[0] ); // node index
 			final l = parseInt( inputs[1] ); // left child
-			final r = parseInt( inputs[2] ); // right child
+			final r = parseInt( inputs[2] ); // right schild
 
-			final node = nodes.exists( p ) ? nodes[p] : new Node( p );
-			nodes.set( p, node );
-			final left = nodes.exists( l ) ? nodes[l] : new Node( l );
-			nodes.set( l, left );
-			final right = nodes.exists( r ) ? nodes[r] : new Node( r );
-			nodes.set( r, right );
-
-			node.left = left;
-			node.right = right;
-			left.parent =  node;
-			right.parent = node;
+			nodes.set( p, { left: l, right: r });
+			parents.set( l, p );
+			parents.set( r, p );
 		}
 
-		final targetNode = nodes[v];
-		if( targetNode.parent == null ) {
+		if( parents.exists( v )) {
+			final result = [];
+			var node = v;
+			var parent = parents[node];
+			do {
+				result.push( nodes[parent].left == node ? "Left" : "Right" );
+				node = parent;
+				parent = parents[parent];
+			} while( parent != null );
+			result.reverse();
+			print( result.join(' '));
+		} else {
 			print( "Root" );
-			return;
 		}
-		
-		final commands:Array<String> = [];
-		var node = targetNode;
-		var parent = targetNode.parent;
-		do {
-			final command = node == parent.left ? "Left" : "Right";
-			commands.push( command );
-			node = parent;
-			parent = node.parent;
-		} while( parent != null );
-
-		commands.reverse();
-		print( commands.join(" "));
 
 	}
 
+}
+
+typedef Node = {
+	final left:Int;
+	final right:Int;
 }
