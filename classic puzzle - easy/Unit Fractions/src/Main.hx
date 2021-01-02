e8eff89f4598cef79bbe5b3d81148dfbef22144c
0xA3 <deslac@protonmail.ch>
2021-01-02 17:57:05 +0100

    add UnitFractions 2nd solution

--- classic puzzle - easy/Unit Fractions/src/Main.hx
+++ classic puzzle - easy/Unit Fractions/src/Main.hx
@@ -1,3 +1,4 @@
+import haxe.Int64;
 import CodinGame.print;
 import CodinGame.printErr;
 import CodinGame.readline;
@@ -10,7 +11,7 @@ class Main {
 		
 		final n = parseInt( readline());
 		
-		final result = process( n );
+		final result = process2( n );
 		print( result );
 	}
 
@@ -22,15 +23,34 @@ class Main {
 	//      ab = n²
 
 	//      if a is divisor of n² then 1/n = 1/(a+n) + 1/(b+n) with b = n²/a
-
 	static function process( n:Int ) {
 
 		final results:Array<String> = [];
 		for( i in 1...n + 1 ) {
 			if(( n * n ) % i == 0) results.push ( '1/$n = 1/${ n * n / i + n } + 1/${ i + n}' );
 		}
-		return results.join( "\n" );
 		
+		return results.join( "\n" );
+	}
+
+	// 1/n = 1/x + 1/y
+	// 1/n - 1/y = 1/x
+	// (y - n)/(yn) = 1/x
+	// unit fraction implies (y-n)|(yn)
+	static function process2( n:Int ) {
+		
+		final results:Array<String> = [];
+		
+		for( y in n + 1...2 * n + 1 ) {
+			final numerator = y - n;
+			final denominator = y * n;
+
+			if( denominator % numerator == 0 ) {
+				results.push( '1/$n = 1/${denominator / numerator} + 1/$y' );
+			}
+		}
+		
+		return results.join( "\n" );
 	}
 
 }
