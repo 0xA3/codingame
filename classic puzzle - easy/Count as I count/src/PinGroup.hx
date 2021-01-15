e18d1c3faa4bf07584da73241735e786b84f42ae
0xA3 <deslac@protonmail.ch>
2021-01-15 20:57:00 +0100

    optimize PinGroup

--- classic puzzle - easy/Count as I count/src/PinGroup.hx
+++ classic puzzle - easy/Count as I count/src/PinGroup.hx
@@ -1,26 +1,17 @@
 class PinGroup {
 	
+	public static inline var PINS = 12;
+
 	public final score:Int;
-	final pins = [for( i in 0...12 ) i + 1];
 
 	public function new( score = 0 ) {
 		this.score = score;
 	}
 
 	public function getChildPinGroups() {
-		final singleHitPinGroups = [for( i in 0...pins.length ) getSingleHitPinGroup( i + 1 )];
-		final multipleHitPinGroups = [for( i in 1...pins.length ) getMultipleHitPinGroup( i + 1 )];
+		final singleHitPinGroups = [for( pin in 0...PINS ) new PinGroup( score + pin + 1 )];
+		final multipleHitPinGroups = [for( pins in 1...PINS ) new PinGroup( score + pins + 1 )];
 		return singleHitPinGroups.concat( multipleHitPinGroups );
-	}
-
-	function getSingleHitPinGroup( pin:Int ) {
-		final nextScore = score + pin;
-		return new PinGroup( nextScore );
-	}
-
-	function getMultipleHitPinGroup( pins:Int ) {
-		final nextScore = score + pins;
-		return new PinGroup( nextScore );
 	}
 
 }
