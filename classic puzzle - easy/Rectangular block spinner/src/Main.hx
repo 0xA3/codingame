2cc598ed023185da11f324ae0d68afef12808cd9
0xA3 <deslac@protonmail.ch>
2021-01-28 11:40:46 +0100

    optimize Rectangular block spinner

--- classic puzzle - easy/Rectangular block spinner/src/Main.hx
+++ classic puzzle - easy/Rectangular block spinner/src/Main.hx
@@ -22,69 +22,35 @@ class Main {
 
 	static function process( size:Int, lines:Array<Array<String>>, angle:Int ) {
 		
-		final realAngle = ( angle % 360 );
-		final size2 = lines.length * 2 - 1;
-
-		var tempAngle = realAngle;
-		while( tempAngle > 90 ) {
-			lines = rotate90( size, lines );
-			tempAngle -= 90;
+		var flip = false;
+		switch angle % 360 {
+			case 45:
+				for( line in lines ) line.reverse();
+				flip = true;
+			case 135:
+				lines.reverse();
+				for( line in lines ) line.reverse();
+			case 225:
+				lines.reverse();
+				flip = true;
+			default: // 315
 		}
-		
-		final lines2 = rotate45( size, lines );
-		final outputLines = [];
-		for( line in lines2 ) {
-			final s = line.join(" ");
-			final spacesNo = int(( size2 - s.length  ) / 2 );
-			final spaces = [for( i in 0...spacesNo ) " "].join( "" );
-			outputLines.push( '$spaces$s$spaces' );
-		}
-		// trace( "\n" + outputLines.join( "\n" ));
 
-		return outputLines.join( "\n" );
-	}
-
-	static function rotate45( size:Int, lines:Array<Array<String>> ) {
-		
+		final size2 = size * 2 - 1;
 		final lines2 = [];
-		
-		// top half
-		for( i in 0...size ) {
-			var x = size - i - 1;
-			var y = 0;
-			final line = [];
-			while( x < size && y < size ) {
-				line.push( lines[y][x] );
-				x++;
-				y++;
+		for( i in 0...size2 ) {
+			final s = [for( _ in 0...size2 ) " "];
+			for( j in 0...i + 1 ) {
+				if( lines[i - j] != null && lines[i - j][j] != null ) {
+					s[size - 1 - i + 2 * j] = lines[i - j][j];
+				}
 			}
-			lines2.push( line );
+			if( flip ) s.reverse();
+			lines2.push( s.join( "" ));
 		}
-		// bottom half
-		for( i in 1...size ) {
-			var x = 0;
-			var y = i;
-			final line = [];
-			while( x < size && y < size ) {
-				line.push( lines[y][x] );
-				x++;
-				y++;
-			}
-			lines2.push( line );
-		}
-		return lines2;
-	}
+		// trace( "\n" + lines2.map( line -> '"' + line + '"' ).join( "\n" ));
+		return lines2.join( "\n" );
 
-	static function rotate90( size:Int, lines:Array<Array<String>> ) {
-		final lines2 = [];
-		for( y in 0...size ) {
-			final line = [];
-			for( x in 0... size ) {
-				line.push( lines[x][size - y - 1 ] );
-			}
-			lines2.push( line );
-		}
-		return lines2;
 	}
 
 }
