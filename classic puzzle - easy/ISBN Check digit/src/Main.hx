e00ddcf3ceed3e8f0a938a50f4e4b4697afd0608
0xA3 <deslac@protonmail.ch>
2021-01-30 14:21:38 +0100

    optimize ISBN Check digit

--- classic puzzle - easy/ISBN Check digit/src/Main.hx
+++ classic puzzle - easy/ISBN Check digit/src/Main.hx
@@ -12,7 +12,7 @@ using StringTools;
 class Main {
 	
 	static final weights10 = [for( i in 0...10 ) 10 - i];
-	static final weights13 = [for( i in 0...12 ) i % 2 == 0 ? 1 : 3];
+	static final weights13 = [for( i in 0...13 ) i % 2 == 0 ? 1 : 3];
 
 	static function main() {
 		
@@ -36,39 +36,33 @@ class Main {
 	}
 
 	static function validate( isbn:String ) {
-		if( isbn.length != 10 && isbn.length != 13 ) return false;
-		return isbn.length == 10 ? validate10( isbn ) : validate13( isbn );
+		if( isbn.length == 10 ) return validate10( isbn );
+		else if( isbn.length == 13 ) return validate13( isbn );
+		else return false;
 	}
 
 	static function validate10( isbn:String ) {
-		final checkValue = isbn.charAt( 9 ) == "X" ? 10 : parseInt( isbn.charAt( 9 ));
-		if( checkValue == null ) return false;
-		// trace( 'validate10 $isbn ${isbn.length}' );
-		
 		var sum = 0;
-		for( i in 0...9 ) {
-			final char = parseInt( isbn.charAt( i ));
+		for( i in 0...isbn.length ) {
+			final char = i == 9 && isbn.charAt( i ) == "X" ? 10 : parseInt( isbn.charAt( i ));
 			if( char == null ) return false;
 			sum += char * weights10[i];
 		}
 		
-		// trace( '$isbn sum $sum checkValue $checkValue ${( sum + checkValue ) % 11}' );
-		return ( sum + checkValue ) % 11 == 0;
+		// trace( '$isbn sum $sum ${sum % 11 == 0}' );
+		return sum % 11 == 0;
 	}
 
 	static function validate13( isbn:String ) {
-		final checkValue = parseInt( isbn.charAt( 12 ));
-		if( checkValue == null ) return false;
-		
 		var sum = 0;
-		for( i in 0...12 ) {
+		for( i in 0...isbn.length ) {
 			final char = parseInt( isbn.charAt( i ));
 			if( char == null ) return false;
 			sum += char * weights13[i];
 		}
 		
-		// trace( '$isbn sum $sum checkValue $checkValue ${( sum + checkValue ) % 10}' );
+		// trace( '$isbn sum $sum ${sum % 10 == 0}' );
 
-		return ( sum + checkValue ) % 10 == 0;
+		return sum % 10 == 0;
 	}
 }
