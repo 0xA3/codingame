0f9d5ca933961038d74d524f814d1061e89b39b5
0xA3 <deslac@protonmail.ch>
2022-02-11 08:56:45 +0100

    move match function to RegexTools

--- compete - code golf/Unary/src/Main.hx
+++ compete - code golf/Unary/src/Main.hx
@@ -1,4 +1,5 @@
 using xa3.StringUtils;
+using xa3.RegexTools;
 using Lambda;
 
 class Main {
@@ -11,13 +12,7 @@ class Main {
 	static function process( input:String ) {
 		final s1 = input.split( "" ).map( c -> "000000" + c.charCodeAt( 0 ).toString( 2 ));
 		final s2 = s1.map( s -> s.substr( -7 )).join( "" );
-		final ereg = ~/(1+|0+)/g;
-		final s3 = [];
-		var m = s2;
-		while( ereg.match( m )) {
-			s3.push( ereg.matched( 1 ));
-			m = ereg.matchedRight();
-		};
+		final s3 = s2.match( ~/(1+|0+)/g );
 		final s4 = s3.map( s -> ( s.charAt( 0 ) == "1" ? "0 " : "00 " )  + "0".repeat( s.length )).join( " " );
 		
 		return s4;
--- compete - code golf/Unary/src/xa3/RegexTools.hx
+++ compete - code golf/Unary/src/xa3/RegexTools.hx
@@ -1,7 +1,13 @@
 package xa3;
 
-function match( s1:String, ereg:EReg ) {
-	return ereg.split( s1 );
+function match( s:String, ereg:EReg ) {
+	final matched = [];
+	var m = s;
+	while( ereg.match( m )) {
+		matched.push( ereg.matched( 1 ));
+		m = ereg.matchedRight();
+	}
+	return matched;
 }
 
 function replace( s1:String, ereg:EReg, s2:String ) {
