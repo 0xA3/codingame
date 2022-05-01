8ab14d1a2dc1c48c7481563fc1d1b1c880e35a8b
0xA3 <deslac@protonmail.ch>
2022-05-01 20:41:41 +0200

    change back to Gold11

--- compete - bot programming/Spring Challenge 2022/src/agent/CurrentAgents.hx
+++ compete - bot programming/Spring Challenge 2022/src/agent/CurrentAgents.hx
@@ -1,6 +1,6 @@
 package agent;
 
 class CurrentAgents {
-	public static final agentMe = new agent.Gold12();
-	public static final agentOpp = new agent.Gold11();	
+	public static final agentMe = new agent.Gold11();
+	public static final agentOpp = new agent.Gold12();	
 }
