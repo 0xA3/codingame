e0a4385de94c22a05652b816a723ae31fa16a3c2
0xA3 <deslac@protonmail.ch>
2022-07-09 13:24:19 +0200

    optimize Retro Typewriter Art

--- classic puzzle - easy/Retro Typewriter Art/src/Main.hx
+++ classic puzzle - easy/Retro Typewriter Art/src/Main.hx
@@ -17,23 +17,26 @@ function main() {
 function process( t:String ) {
 	final commands = t.split(" ").map( chunk -> parseChunk( chunk ));
 
-	final chars = commands.map( command -> [for( _ in 0...command.repetitions ) command.char].join( "" ));
+	final chars = commands.map( command -> [for( _ in 0...command.reps ) command.char].join( "" ));
 	final output = chars.join( "" );
 
 	return output;
 }
 
 function parseChunk( chunk:String ):Command {
-	if( chunk == "nl" ) return { char: "\n", repetitions: 1 }
-	if( chunk.contains( "sp" )) return { char: " ", repetitions: parseInt( chunk.substr( 0, chunk.length - 2 ))}
-	if( chunk.contains( "bS" )) return { char: "\\", repetitions: parseInt( chunk.substr( 0, chunk.length - 2 ))}
-	if( chunk.contains( "sQ" )) return { char: "'", repetitions: parseInt( chunk.substr( 0, chunk.length - 2 ))}
-	return { char: chunk.substr( chunk.length - 1 ), repetitions: parseInt( chunk.substr( 0, chunk.length - 1 ))}
+	if( chunk == "nl" ) return { char: "\n", reps: 1 }
+	if( chunk.contains( "sp" )) return { char: " ", reps: getReps2( chunk )}
+	if( chunk.contains( "bS" )) return { char: "\\", reps: getReps2( chunk )}
+	if( chunk.contains( "sQ" )) return { char: "'", reps: getReps2( chunk )}
+	return { char: chunk.substr( chunk.length - 1 ), reps: getReps1( chunk )}
 }	
+
+function getReps1( s:String ) return parseInt( s.substr( 0, s.length - 1 ));
+function getReps2( s:String ) return parseInt( s.substr( 0, s.length - 2 ));
 
 
 typedef Command = {
 	final char:String;
-	final repetitions:Int;
+	final reps:Int;
 }
 
