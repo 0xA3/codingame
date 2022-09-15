9d69a5c01fe149467b16514a54d10b4a626706ff
0xA3 <deslac@protonmail.ch>
2022-09-15 11:35:44 +0200

    add reverse n times n - 2

---
+++ compete - clash of code/reverse/reverse - n times n - 2/src/Main.hx
@@ -0,0 +1,12 @@
+import CodinGame.print;
+import CodinGame.printErr;
+import CodinGame.readline;
+import Std.int;
+import Std.parseFloat;
+
+function main() {
+
+	final n = parseFloat( readline());
+	
+	print( n * ( n - 2 ));
+}
