e1f63ac8e2875f64056014488408566306dfc1ce
0xA3 <deslac@protonmail.ch>
2022-10-23 10:20:00 +0200

    add lib to template shortest

--- compete - clash of code/template/template shortest/src/Main.hx
+++ compete - clash of code/template/template shortest/src/Main.hx
@@ -3,6 +3,14 @@ import CodinGame.printErr;
 import CodinGame.readline;
 import Std.int;
 import Std.parseInt;
+import xa3.MathUtils;
+
+using Lambda;
+using StringTools;
+using xa3.NumberConvert;
+using xa3.NumberFormat;
+using xa3.RegexUtils;
+using xa3.StringUtils;
 
 /*
 
