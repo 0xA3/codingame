69ca3986323c66af33edd56dab0d9aa217b2cc73
0xA3 <deslac@protonmail.ch>
2022-10-24 14:48:22 +0200

    fix error in Blending Colors

--- classic puzzle - easy/Blending Colors/src/shape/Square.hx
+++ classic puzzle - easy/Blending Colors/src/shape/Square.hx
@@ -5,7 +5,7 @@ class Square extends Shape {
 	override function pointIsOnBorder( point:Point ) {
 		final isOnTopBorder = point.y == y && point.x >= x && point.x <= x + s;
 		final isOnLeftBorder = point.x == x && point.y >= y && point.y <= y + s;
-		final isOnBottomBorder = point.y == y && point.x >= x && point.x <= x + s;
+		final isOnBottomBorder = point.y == y + s && point.x >= x && point.x <= x + s;
 		final isOnRightBorder = point.x == x + s && point.y >= y && point.y <= y + s;
 		// trace( 'check Square $this  pointOnBorder   ${isOnTopBorder || isOnLeftBorder || isOnBottomBorder || isOnRightBorder}' );
 		return isOnTopBorder || isOnLeftBorder || isOnBottomBorder || isOnRightBorder;
