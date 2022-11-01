abd9f4b41e0aa0095b9510c856ef2bd5052e883d
0xA3 <deslac@protonmail.ch>
2022-11-01 10:32:48 +0100

    add Match DNA Sequence

--- classic puzzle - easy/Match DNA Sequence/src/Main.hx
+++ classic puzzle - easy/Match DNA Sequence/src/Main.hx
@@ -18,5 +18,24 @@ function main() {
 
 function process( delta:Int, gene:String, chrs:Array<String> ) {
 
-	return "";
+	for( i in 0...chrs.length ) {
+		final chr = chrs[i];
+		for( o in 0...chr.length + 1 - gene.length + delta ) {
+			final errors = countErrors( gene, chr.substr( o ));
+			if( errors <= delta ) {
+				return '$i $o $errors';
+			}
+		}
+	}
+	return "NONE";
+}
+
+function countErrors( s1:String, s2:String ) {
+	var errors = 0;
+	for( i in 0...s1.length ) {
+		final c1 = s1.charAt( i );
+		final c2 = s2.length < i ? "" : s2.charAt( i );
+		if( c1 != c2 ) errors++;
+	}
+	return errors;
 }
