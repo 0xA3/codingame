ab87e6d2449d92cec338a9d3f962a9c453ffa7aa
0xA3 <deslac@protonmail.ch>
2022-11-23 13:46:24 +0100

    add regex solution

--- classic puzzle - easy/Card Counting when easily distracted/src/Main.hx
+++ classic puzzle - easy/Card Counting when easily distracted/src/Main.hx
@@ -25,8 +25,9 @@ function process( streamOfConsciousness:String, bustThreshold:Int ) {
 	for( i in 2...10 ) cardValues.set( '$i', i );
 	final cardsInGame = [for( card in cardValues.keys()) card => 4];
 
-	final words = streamOfConsciousness.split( "." ).map( s -> s.split( "" ));
-	final cards = words.filter( word -> isCardSeries( word )).flatten();
+	final words = streamOfConsciousness.split( "." );
+	final ereg = ~/[^2-9TJQKA]/g;
+	final cards = words.filter( word -> !ereg.match( word )).flatMap( word -> word.split( "" ));
 	for( card in cards ) cardsInGame[card]--;
 
 	final totalCardsInGame = [for( number in cardsInGame ) number].fold(( v, sum ) -> sum + v, 0 );
@@ -36,19 +37,4 @@ function process( streamOfConsciousness:String, bustThreshold:Int ) {
 	final winPercentage = round( winChance * 100 );
 	
 	return '$winPercentage%';
-}
-
-function isCardSeries( word:Array<String> ) {
-	for( char in word ) if( !cardValues.exists( char )) return false;
-	return true;
-}
-
-function getMatches( s:String, eReg:EReg, index = 1 ) {
-	final matched = [];
-	var input = s;
-	while( eReg.match( input )) {
-		matched.push( eReg.matched( index ));
-		input = eReg.matchedRight();
-	}
-	return matched;
 }
