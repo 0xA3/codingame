72881edd2ed2019fee700c560c5b395a012e80aa
0xA3 <deslac@protonmail.ch>
2023-03-06 19:02:46 +0100

    change variable declaration

--- solo - classic puzzle - easy/Onboarding_CPP/src/Main.hx
+++ solo - classic puzzle - easy/Onboarding_CPP/src/Main.hx
@@ -5,13 +5,15 @@ class Main {
 		while( true ) {
 			var enemy1:String = "";
 			CodinGame.readline( enemy1 ); // name of enemy 1
-			var dist1:Int = 0;
-			CodinGame.readline( dist1 ); // distance to enemy 1
-			
+			var dist1Str = "";
+			CodinGame.readline( dist1Str ); // distance to enemy 1
+			final dist1 = Std.parseInt( dist1Str );
+
 			var enemy2:String = "";
 			CodinGame.readline( enemy2 ); // name of enemy 2
-			var dist2:Int = 0;
-			CodinGame.readline( dist2 ); // distance to enemy 2
+			var dist2Str:String = "";
+			CodinGame.readline( dist2Str ); // distance to enemy 2
+			final dist2 = Std.parseInt( dist2Str );
 
 			final enemy = dist1 < dist2 ? enemy1 : enemy2;
 			CodinGame.print( enemy );
