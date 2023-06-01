a40ec19756abad1f53c70b41f9cbf670b4686924
0xA3 <deslac@protonmail.ch>
2023-06-01 18:40:17 +0200

    move search to Ai class

--- multi - bot programming/Spring Challenge 2023/src/ai/versions/Ai1.hx
+++ multi - bot programming/Spring Challenge 2023/src/ai/versions/Ai1.hx
@@ -1,10 +1,10 @@
 package ai.versions;
 
 import CodinGame.printErr;
-import ai.algorithm.BreadthFirstSearch;
 import ai.data.CellDataset;
 import ai.data.CellDistance;
 import ai.data.FrameCellDataset;
+import ai.data.Node;
 
 using Lambda;
 
@@ -44,7 +44,7 @@ class Ai1 implements IAi {
 		
 		var output = "";
 		for( myBaseIndex in myBaseIndices ) {
-			final cellDistances = BreadthFirstSearch.getCellDistances( cells, myBaseIndex );
+			final cellDistances = getCellDistances( cells, myBaseIndex );
 			cellDistances.sort(( a, b ) -> a.distance - b.distance );
 			final closestCell = cellDistances[0];
 			output += 'LINE ${closestCell.start} ${closestCell.end} 1';
@@ -54,4 +54,44 @@ class Ai1 implements IAi {
 		
 		return output;
 	};
+
+	function getCellDistances( cells:Array<CellDataset>, start:Int ) {
+		final nodes = [for( _ in cells ) new Node()];
+		final frontier = new List<Int>();
+		
+		frontier.add( start );
+		nodes[start].visited = true;
+
+		final resourceCellDistances:Array<CellDistance> = [];
+		while( !frontier.isEmpty()) {
+			final current = frontier.pop();
+			if( cells[current].resources > 0 ) {
+				final distance = getDistance( nodes, start, current );
+				final cellDistance:CellDistance = { start: start, end: current, distance: distance }
+				resourceCellDistances.push( cellDistance );
+			}
+			// trace( 'current $current' );
+			for( neighbor in cells[current].neighbors ) {
+				// trace( 'check $next' );
+				final nextNode = nodes[neighbor];
+				if( !nextNode.visited ) {
+					nextNode.previous = current;
+					nextNode.visited = true;
+					frontier.add( neighbor );
+				}
+			}
+		}
+		return resourceCellDistances;
+	}
+
+	function getDistance( nodes:Array<Node>, start:Int, end:Int ) {
+		var distance = 0;
+		var i = end;
+		while( i != start ) {
+			i = nodes[i].previous;
+			distance++;
+		}
+		return distance;
+	}
+
 }
