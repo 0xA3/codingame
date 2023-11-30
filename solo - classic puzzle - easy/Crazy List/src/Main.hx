b7a944c09c93f9d2d6d5e99b263d664cb4470663
0xA3 <deslac@protonmail.ch>
2023-11-30 15:28:37 +0100

    add formulas

--- solo - classic puzzle - easy/Crazy List/src/Main.hx
+++ solo - classic puzzle - easy/Crazy List/src/Main.hx
@@ -22,6 +22,24 @@ function process( crazyList:String ) {
 	final u2 = s[1];
 	final u3 = s[2];
 
+	/*
+	u2 = a * u1 + b
+	u3 = a * u2 + b
+
+	b = u2 - a * u1
+	b = u3 - a * u2
+
+ 	u2 - a * u1 = u3 - a * u2
+	a * u1 = u3 - a * u2 - u2
+	0 = u3 - a * u2 - u2 - a * u1
+	0 = a * u2 - a * u1 + u3 - u2
+	0 = a * ( u2 - u1 ) + u3 - u2
+	a = ( u3 - u2 ) / ( u2 - u1 )
+
+	b = u2 - a * u1
+
+	*/
+
 	final a = ( u3 - u2 ) / ( u2 - u1 );
 	final b = u2 - a * u1;
 
