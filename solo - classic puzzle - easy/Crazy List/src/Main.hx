9f769da082e62377082dbe02e3d871ce49f2e120
0xA3 <deslac@protonmail.ch>
2023-11-30 10:55:25 +0100

    add solution of Crazy List

--- solo - classic puzzle - easy/Crazy List/src/Main.hx
+++ solo - classic puzzle - easy/Crazy List/src/Main.hx
@@ -14,7 +14,19 @@ function main() {
 }
 
 function process( crazyList:String ) {
-	final numbers = crazyList.split(" ").map( s -> parseInt( s ));
+	final s = crazyList.split(" ").map( s -> parseInt( s ));
+
+	if( s[0] == s[1] ) return s[0];
+
+	final u1 = s[0];
+	final u2 = s[1];
+	final u3 = s[2];
+
+	final a = ( u3 - u2 ) / ( u2 - u1 );
+	final b = u2 - a * u1;
+
+	final last = s[s.length - 1];
+	final nextNumber = int( a * last + b );
 	
-	return 0;
+	return nextNumber;
 }
