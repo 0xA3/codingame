c501e1cc2c7a4880313a4098371780da403f5af1
0xA3 <deslac@protonmail.ch>
2024-07-31 13:59:47 +0200

    add simplified version of zeckendorf

--- solo - classic puzzle - easy/Zeckendorf representation part 1/src/Main.hx
+++ solo - classic puzzle - easy/Zeckendorf representation part 1/src/Main.hx
@@ -15,41 +15,21 @@ function main() {
 }
 
 function process( n:Int64 ) {
+	final fibs = [1i64, 2i64];
+	while( fibs[fibs.length - 1] < n ) {
+		fibs.push( fibs[fibs.length - 1] + fibs[fibs.length - 2] );
+	}
 
-	final fibSeq:Array<Int64> = fibonacci( n );
-	fibSeq.reverse();
+	fibs.reverse();
 
-	final outputSeq = getZeckendorfRepresentation( n, fibSeq, [] );
-	return outputSeq.map( v -> '$v' ).join( "+" );
-}
-
-function getZeckendorfRepresentation( n:Int64, inputSeq:Array<Int64>, outputSeq:Array<Int64> ) {
-	final previousSum = outputSeq.sum();
-	// trace( 'n: $n, outputSeq: ${outputSeq.map( v -> '$v')}' );
-	for( i in 0...inputSeq.length ) {
-		final currentFib = inputSeq[i];
-		// trace( 'currentFib: ${currentFib}, sum: ${previousSum + currentFib}' );
-		final currentSum = previousSum + currentFib;
-		if( currentSum < n ) {
-			return getZeckendorfRepresentation( n, inputSeq.slice( 2, inputSeq.length ), outputSeq.copy().concat( [currentFib] ));
-		} else if( currentSum == n ) {
-			outputSeq.push( currentFib );
-			return outputSeq;
+	final outputs = [];
+	var tempN = n;
+	for( f in fibs ) {
+		if( f <= tempN ) {
+			outputs.push( f );
+			tempN -= f;
 		}
 	}
-	throw 'Error: Sequence not found';
-}
 
-// create fibbonaci sequence
-function fibonacci( max:Int64 ) {
-    var fibSeq = [0i64, 1i64];
-    var i = 2;
-	while( true ) {
-        final fib = fibSeq[i - 2] + fibSeq[i - 1];
-		if( fib >= max ) break;
-
-		fibSeq.push( fib );
-		i++;
-    }
-    return fibSeq;
+	return outputs.map( v -> '$v' ).join( "+" );
 }
