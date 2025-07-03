7ffea27f1cd43800a035b22396f5d31a2268c394
0xA3 <deslac@protonmail.ch>
2025-07-03 20:36:43 +0200

    await inputs

--- multi - bot programming/Winter Challenge 2024 - Cellularena/src/ai/MainAiLocal.hx
+++ multi - bot programming/Winter Challenge 2024 - Cellularena/src/ai/MainAiLocal.hx
@@ -8,8 +8,8 @@ using tink.CoreApi;
 // below is a mechanism that connects nodejs standard input with something that resembles the readline()
 @await class MainAiLocal {
 	
-	static var stdInLines:Array<String> = [];
-	static var promises:Array<Dynamic> = [];
+	static var inputLines:Array<String> = [];
+	static var promises:Array<(String)->Void> = [];
 	static var buffer:String = "";
 
 	static function main() {
@@ -20,7 +20,7 @@ using tink.CoreApi;
 			buffer += data;
 			var lines:Array<String> = buffer.split( "\n" );
 			if( lines.length > 1 ) {
-				for( i in 0...lines.length - 1 ) stdInLines.push( lines[i].trim() );
+				for( i in 0...lines.length - 1 ) inputLines.push( lines[i].trim() );
 				buffer = lines[lines.length - 1];
 				onReadline();
 			}
@@ -33,30 +33,40 @@ using tink.CoreApi;
 	static function onReadline() {
 		while( true ) {
 			if( promises.length == 0 ) break;
-			if( stdInLines.length == 0 ) break;
+			if( inputLines.length == 0 ) break;
 			var prom = promises.shift();
-			var str = stdInLines.shift();
+			var str = inputLines.shift();
+			
 			prom( str );
 		}
 	}
 
 	static function readline() {
-		var resolve:Dynamic;
+		// trace( "readline" );
+		var resolve:(String)->Void;
 
-		final initFunc = function( resolveFunc, rejectFunc ):Void {
-			resolve = resolveFunc;
+		final initFunc = function( r:(String)->Void ):Void {
+			resolve = r;
+			// trace( 'initFunc resolve: ${resolve}' );
+			promises.push( resolve );
+			onReadline();
 		}
 
-		var prom:js.lib.Promise<String> = new js.lib.Promise( initFunc );
-		promises.push( resolve );
-		onReadline();
+		// var prom:js.lib.Promise<String> = new js.lib.Promise( initFunc );
+		final prom = Future.irreversible( initFunc );
 		
 		return prom;
 	}
 
 	@await static function asyncMain() {
 		// actual code, but must use (await readline()) instead of readline() - for example
-		var input = @await readline();
+		// trace( "asyncMain" );
+		var input1 = @await readline();
+		trace( 'input1: ${input1}' );
+		var input2 = @await readline();
+		trace( 'input2: ${input2}' );
+		
+		
 		trace( "WAIT" );
 	}
 }
