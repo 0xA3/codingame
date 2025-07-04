5ba3c93351a118a0292544ae04798d912c29801f
0xA3 <deslac@protonmail.ch>
2025-07-04 13:12:11 +0200

    clean up

---
+++ multi - bot programming/Winter Challenge 2024 - Cellularena/build.bat
@@ -0,0 +1,1 @@
+astra build ".\dest\ai_local.js" -o ".\build" -n "node_v24.0.2-win-x64"
--- multi - bot programming/Winter Challenge 2024 - Cellularena/run.bat
+++ multi - bot programming/Winter Challenge 2024 - Cellularena/run.bat
@@ -1,5 +1,4 @@
 REM runs winter challenge, which executes 2 bot programs and runs a server for website results
 REM first, go back to main dir (must run from main project dir, WinterChallenge2024-Cellularena)
-cd..
-java -jar ./build/winter-2024.jar -p1 "build/ai.exe" -p2 "build/ai_wait.exe" -s -l "log.txt"
-# winter-2024.jar from https://github.com/httpsx/WinterChallenge2024-Cellularena
+REM winter-2024.jar from https://github.com/httpsx/WinterChallenge2024-Cellularena
+java -jar ./build/winter-2024.jar -p1 "build/build.exe" -p2 "build/ai_wait.exe" -s -l "log.txt"
