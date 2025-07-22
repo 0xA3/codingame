949d5f83cfd7404fceb3f65795208c1c9eb2d432
0xA3 <deslac@protonmail.ch>
2025-07-22 16:45:04 +0200

    add TAgent

---
+++ multi - bot programming/Summer Challenge 2025 - Soak overflow/src/ai/data/TAgent.hx
@@ -0,0 +1,15 @@
+package ai.data;
+/*
+GUNNER(1, 16, 4, 1),
+SNIPER(5, 24, 6, 0),
+BOMBER(2, 8, 2, 3),
+ASSAULT(2, 16, 4, 2),
+BERSERKER(5, 32, 2, 1);
+*/
+enum TAgent {
+	Gunner;
+	Sniper;
+	Bomber;
+	Assault;
+	Berserker;
+}
