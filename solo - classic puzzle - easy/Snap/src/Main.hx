16965e49b00d78b699337c9a24fed90217adb1a1
0xA3 <deslac@protonmail.ch>
2026-02-24 12:15:52 +0100

    change Snap

--- solo - classic puzzle - easy/Snap/src/Main.hx
+++ solo - classic puzzle - easy/Snap/src/Main.hx
@@ -27,31 +27,27 @@ function process( cardsPlayers:Array<Array<String>> ) {
 		if( cardsPlayers[1 - i].length == 0 ) return 'Winner: Player ${i + 1}\n${cardsPlayers[i].length}';
 		centralCards.unshift( cardsPlayers[i].shift() );
 
-		var isSnap = false;
+		var switchPlayers = true;
 		if( centralCards.length > 1 ) {
 			final card0 = centralCards[0];
 			final card1 = centralCards[1];
 			final rank1 = card0.charAt( 0 );
 			final rank2 = card1.charAt( 0 );
 			if( rank1 == rank2 ) {
-				isSnap = true;
 				final suits = [card0, card1].map( c -> c.charAt( c.length - 1 ));
 				final precedence = suitsValue[suits[0]] > suitsValue[suits[1]] ? 0 : 1;
 				centralCards.reverse();
 				if( precedence == 0 ) {
 					cardsPlayers[i] = cardsPlayers[i].concat( centralCards );
+					switchPlayers = false;
 				} else {
 					cardsPlayers[1 - i] = cardsPlayers[1 - i].concat( centralCards );
-					i = 1 - i;
 				}
 				centralCards.splice( 0, centralCards.length );
 			}
 		}
-		if( !isSnap ) i = 1 - i;
+		if( switchPlayers ) i = 1 - i;
 	}
 
 	throw 'Error: no winner';
-}
-
-function getPrecedenceId( cards:Array<String> ) {
 }
